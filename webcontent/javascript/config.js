06af5acd9e16057780a4cd7aad861cd071011d46
Andreas Hölzl <andy.woodly@googlemail.com>
2012-05-19 17:03:46 +0200

    refactored config out

---
+++ webcontent/javascript/config.js
@@ -0,0 +1,14 @@
+/*
+ Copyright © Andreas Hölzl
+ @author twitter.com/andywoodly
+*/
+
+var config = (function(){
+
+    this.baseUrl = 'https://www.googleapis.com/calendar/v3/calendars/';
+    this.calId = 'a0i2n11sc0e74d813ishbpiiso%40group.calendar.google.com';
+    this.apiKey = 'AIzaSyBxpeKeOwcenFKoYcjA4wMeVCEPdpN7GRo';
+    this.options = 'singleEvents=true';
+    this.timezone = 'Europe/Paris';
+
+})();
